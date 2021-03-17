aedde83386b0e6c60b0ce013dc595109bbc6599d
Jeff Calder <jcalder@umn.edu>
2021-03-17 10:29:31 -0500

    Fixed svi_demo.py script

--- examples/svi_demo.py
+++ examples/svi_demo.py
@@ -8,17 +8,17 @@ def emph_colors(S):
     return 1-exposure.equalize_hist(C/np.max(C),nbins=1000)
 
 #Load mesh
-mesh = tm.mesh('dragon.ply')
+mesh = tm.load_ply('dragon.ply')
 
 #SVI
 r = [.5,1] #radius of computation - must be array
 vol,gamma = mesh.svi(r)
 mlab.figure()
-mlab.triangular_mesh(mesh.Points[:,0],mesh.Points[:,1],mesh.Points[:,2],mesh.Triangles,scalars=emph_colors(vol[:,0]))    
+mlab.triangular_mesh(mesh.points[:,0],mesh.points[:,1],mesh.points[:,2],mesh.triangles,scalars=emph_colors(vol[:,0]))    
 
 #SVIPCA
 r = [1]
 S,K1,K2,V1,V2,V3=mesh.svipca(r)
 mlab.figure()
-mlab.triangular_mesh(mesh.Points[:,0],mesh.Points[:,1],mesh.Points[:,2],mesh.Triangles,scalars=emph_colors(K1[:,0]))    
+mlab.triangular_mesh(mesh.points[:,0],mesh.points[:,1],mesh.points[:,2],mesh.triangles,scalars=emph_colors(K1[:,0]))    
 
