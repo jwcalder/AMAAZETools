18045012ddff291345ea07cae76ea44776abfd31
oneil571 <oneil571@umn.edu>
2024-02-20 22:30:55 -0600

    parallelized dicom surfacing + added error support
    parallelized dicom surfacing and added better error handling (e.g. if scan is cut off or bone is absent from where it's supposed to be or surfacing fails for any reason - it will be added to a csv list and saved to the  end): see surface_bones_parallel function.

--- amaazetools/dicom.py
+++ amaazetools/dicom.py
@@ -869,3 +869,76 @@ def seg_adjacency_matrix(u):
 
     return M,X,Y
 
+
+def surfacing_subproc(filename,directory,iso_level,write_gif=False):
+    
+    print('Loading '+filename+'...')
+    M = np.load(os.path.join(directory,filename))
+    I = M['I']; dx = M['dx']; dz = M['dz']
+    
+    #Rescale image to account for different dx/dz dimensions
+    J = rescale(I.astype(float),(dz/dx,1,1),mode='constant')
+    
+    try: 
+        verts,faces,normals,values = measure.marching_cubes(J,iso_level)
+        mesh = tm.mesh(dx*verts,faces) #Multiplication by dx fixes units
+    
+        #Reverse orientation of triangles (marching_cubes returns inward normals)
+        mesh.flip_normals()
+    
+        #Write to ply file
+        mesh_filename = os.path.join(directory,filename[:-4]+'_iso%d'%iso_level)
+        print('Saving mesh to '+mesh_filename+'...')
+        mesh.to_ply(mesh_filename+'.ply')
+    
+        if write_gif:
+            mesh.to_gif(mesh_filename+'.gif')
+        return '0'
+    except Exception as error:
+        print('surfacing error with ', filename, ': ', error)
+        return filename
+
+
+
+def surface_bones_parallel(directory, iso=2500, write_gif=False,error_fname='./surfacing_errors.csv',ncores='all'):
+    """ parallelized implementation of surface_bones with also surfacing error support.
+        Processes all npz files in directory creating surface and saving to a ply file.
+
+        Parameters
+        ----------
+        directory : str
+            Directory to work within.
+        iso : float (optional), default is 2500
+            Iso level to be used for surfacing.
+        write_gif : bool (optional), default=False
+            Whether to output rotating gifs for each object. Requires mayavi, which can be hard to install.
+        error_fname
+
+        Returns
+        -------
+        None
+    """
+    
+    ddd = os.listdir(directory)
+    
+    fnames = []
+    for f in ddd:
+        if f.endswith('.npz'):
+            fnames.append(f)
+    
+        
+    if isinstance(ncores,int):
+        num_cores = ncores
+    else: 
+        num_cores =multiprocessing.cpu_count()
+        
+    errs = Parallel(n_jobs=num_cores)(delayed(surfacing_subproc)(f,directory,iso,write_gif) for f in fnames)
+    
+    errs = np.array(errs)
+    errs = errs[errs!='0']
+    
+    if len(errs)==0:
+        print('no errors, not saving an error csv.')
+    else:
+        print('there were ' + str(len(errs)) +' errors. saving CSV to ',error_fname)              
+        pd.DataFrame(errs).to_csv(error_fname,header=False, index=False)
